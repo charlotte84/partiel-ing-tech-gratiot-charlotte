a4f24d10ef2c152b23bba7216a393170e5305efb
charlotte gratiot Fleurant <charlottefleurant17@gmail.com>
2023-04-20 10:54:23 +0200

    j'ai fait exercice 1 du partiel ing tech je le replace dans le bon ripo j'avais fais l'erreur de le mettre dans le meme ripo que le cms

---
+++ exo-x/app.js
@@ -0,0 +1,17 @@
+class avion {
+  constructor(marque, modele, longueur, nbPlace, reservoir, vitesse){
+    this.marque = marque;
+    this.modele = modele;
+    this.longueur = longueur;
+    this.nbPlace = nbPlace;
+    this.reservoir = reservoir;
+    this.vitesse = vitesse;
+  }
+}
+
+
+//Ici, nous définissons une classe nommée "avion".
+//à partir des arguments: marque, modele, longueur, nbPlace, reservoir, vitesse
+//Chaque propriété est créée comme une propriété de l'objet en utilisant le mot-clé "this".
+
+
